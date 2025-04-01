26b6091a5cc16cc6ffab00f5c495aa7dafb1db85
AlexNagore <alexnagoreiri@gmail.com>
2025-04-01 12:36:52 +0200

    [] - Refactor, variable number en ternario

--- src/ListaCompra.php
+++ src/ListaCompra.php
@@ -8,12 +8,7 @@ class ListaCompra
     {
         $response = explode(" " , $string);
         $product = $response[1];
-        if (count($response) == 3){
-            $number = $response[2];
-        }
-        else {
-            $number = 1;
-        }
+        $number = count($response) == 3 ? $response[2] : 1;
 
         return $product . " x". $number;
     }
